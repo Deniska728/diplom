39026586e75c9ae68bb3001569575752e697208c
Denis Rabov <denis@milkandcartoons.com>
2020-06-16 16:59:58 +0300

    Update legal page

--- src/components/legal/LegalPage.jsx
+++ src/components/legal/LegalPage.jsx
@@ -5,46 +5,44 @@ import showdown from 'showdown';
 const converter = new showdown.Converter();
 
 const text = `
-#Пользовательское Соглашение
+#User Agreement
 
-Настоящее Пользовательское Соглашение (Далее Соглашение) регулирует отношения между ИП Алешковский Н.С. (далее GraphQQ или Администрация) с одной стороны и пользователем сайта с другой.
-Сайт GraphQQ не является средством массовой информации.
+This User Agreement (hereinafter referred to as the Agreement) regulates the relations between FE Aleshkovsky N.S. (hereinafter referred to as GraphQQ or Administration) on the one hand and the site user on the other.
+The GraphQQ website is not a mass medium.
 
-Используя сайт, Вы соглашаетесь с условиями данного соглашения.   
-**Если Вы не согласны с условиями данного соглашения, не используйте сайт GraphQQ!**
+By using the site, you agree to the terms of this agreement.  
+**If you do not agree to the terms of this agreement, do not use the GraphQQ website!**
 
-##Предмет соглашения
-**Администрация предоставляет пользователю право на размещение на сайте следующей информации:**
+##Subject matter of the agreement
+**The administration grants the user the right to place the following information on the site:**
 
-##Предмет соглашения
-**Администрация предоставляет пользователю право на размещение на сайте следующей информации:**
-- Текстовой информации
+##Subject matter of the agreement
+**The administration grants the user the right to place the following information on the site:**
+- Text information
 
-##Права и обязанности сторон
-**Пользователь имеет право:**
-- осуществлять поиск информации на сайте
-- получать информацию на сайте
-- создавать информацию для сайта
-- использовать информацию сайта в личных некоммерческих целях
+##Rights and obligations of the parties
+**The user has the right:**
+- web search
+- provide information on site
+- provide information for the site
+- use this site information for personal, non-commercial purposes
 
-##Администрация имеет право:
+**The user undertakes:**
+- stay tuned
+- not to use scripts (programs) for automated data collection and/or interaction with the Site and its Services
 
-**Пользователь обязуется:**
-- не нарушать работоспособность сайта
-- не использовать скрипты (программы) для автоматизированного сбора информации и/или взаимодействия с Сайтом и его Сервисами
+**The Administration undertakes:**
+- maintain the site's operability, except in cases when it is impossible for reasons beyond the Administration's control.
 
-Администрация обязуется:
-- поддерживать работоспособность сайта за исключением случаев, когда это невозможно по независящим от Администрации причинам.
+##Liability of the parties
+- the administration does not bear any responsibility for services provided by third parties
+- in the event of a force majeure situation (hostilities, state of emergency, natural disaster, etc.) the Administration does not guarantee the safety of the information posted by the User, as well as uninterrupted operation of the information resource
 
-##Ответственность сторон
-- администрация не несет никакой ответственности за услуги, предоставляемые третьими лицами
-- в случае возникновения форс-мажорной ситуации (боевые действия, чрезвычайное положение, стихийное бедствие и т. д.) Администрация не гарантирует сохранность информации, размещённой Пользователем, а также бесперебойную работу информационного ресурса
-
-##Условия действия Соглашения
-Данное Соглашение вступает в силу при регистрации на сайте.
-Соглашение перестает действовать при появлении его новой версии.
-Администрация оставляет за собой право в одностороннем порядке изменять данное соглашение по своему усмотрению.
-Администрация не оповещает пользователей об изменении в Соглашении.
+##Terms and conditions of the Agreement
+This Agreement comes into force upon registration on the site.
+The Agreement ceases to be in force when a new version of the Agreement is available.
+The Administration reserves the right to unilaterally change this Agreement at its sole discretion.
+Administration does not notify users about changes in the Agreement.
 `;
 
 const LegalPage = () => {
